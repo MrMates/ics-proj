e033c71122abd0e8f721d0e91cd1b8882e2f5ab1
David Zoubek <xzoube02@vutbr.cz>
2023-03-04 20:23:29 +0100

    DbContext Factory

---
+++ Project/Project.DAL/Factories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Factories
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+    {
+        private readonly DbContextSqlLiteFactory _dbContextSqlLiteFactory;
+        private const string ConnectionString = $"Data Source=Project;Cache=Shared";
+        public DesignTimeDbContextFactory()
+        {
+            _dbContextSqlLiteFactory = new DbContextSqlLiteFactory(ConnectionString);
+        }
+        public DatabaseContext CreateDbContext(string[] args)
+            => _dbContextSqlLiteFactory.CreateDbContext();
+    }
+}
