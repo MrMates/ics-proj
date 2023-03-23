9ec6a054e68745a3f9564a2e70ac444d856be215
Miky537 <xkopci01@vutbr.cz>
2023-03-23 21:30:42 +0100

    Renamed database.cs, removed [Key] from Activity and added modifications to Activity Time

--- Project/Project.DAL/Entities/UserProject.cs
+++ Project/Project.DAL/Entities/UserProject.cs
@@ -4,7 +4,6 @@ namespace Project.DAL
 {
     public record UserProject : IEntity
     {
-        [Key]
         public required Guid Id { get; set; }
         public required Guid UserId { get; set; }
         public required Guid ProjectId { get; set; }
