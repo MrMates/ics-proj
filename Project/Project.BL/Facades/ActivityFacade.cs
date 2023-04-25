75cb89de1695fe6a8a3daba6dae66bd6d5f2baea
Matěj Toul <mates097@seznam.cz>
2023-04-25 21:25:50 +0200

    Added Activity filter facades and tests for them TODO: Custom filters

--- Project/Project.BL/Facades/ActivityFacade.cs
+++ Project/Project.BL/Facades/ActivityFacade.cs
@@ -3,6 +3,8 @@ using Project.DAL;
 using Project.BL.Models;
 using Project.DAL.Mappers;
 using Project.BL.Mappers;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Project.BL.Facades;
 
@@ -14,5 +16,96 @@ public class ActivityFacade :
         IActivityModelMapper modelMapper)
         : base(unitOfWorkFactory, modelMapper)
     {
+    }
+
+    /// <summary>
+    /// Returns a collection of activities that began (!) in the past week.
+    /// </summary>
+    /// <remarks>Past week is considered from the last Monday, not last 7 days.</remarks>
+    /// <returns>Collection of ActivityListModels that began in the past week.</returns>
+    public async Task<IEnumerable<ActivityListModel>> GetPastWeek()
+    {
+        DateTime dt = DateTime.Now;
+
+        int diff = (7 + (dt.DayOfWeek - DayOfWeek.Monday)) % 7;
+        DateTime lastMonday = dt.AddDays(-1 * diff).Date;
+
+        // Just to check we don't have any "future" activities
+        DateTime endOfWeek = lastMonday.AddDays(7).AddSeconds(-1);
+
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        List<Activity> entities = await uow
+            .GetRepository<Activity, ActivityEntityMapper>()
+            .Get()
+            .Where(activity => activity.TimeBegin >= lastMonday && activity.TimeBegin <= endOfWeek)
+            .ToListAsync();
+
+        return ModelMapper.MapToListModel(entities);
+    }
+
+    /// <summary>
+    /// Returns a collection of activities that began (!) in the past month.
+    /// </summary>
+    /// <remarks>Past month is considered from the first day, not last 30/31 days.</remarks>
+    /// <returns>Collection of ActivityListModels that began in the past month.</returns>
+    public async Task<IEnumerable<ActivityListModel>> GetPastMonth()
+    {
+        DateTime dt = DateTime.Now;
+
+        var firstDayOfMonth = new DateTime(dt.Year, dt.Month, 1);
+        var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddSeconds(-1);
+
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        List<Activity> entities = await uow
+            .GetRepository<Activity, ActivityEntityMapper>()
+            .Get()
+            .Where(activity => activity.TimeBegin >= firstDayOfMonth && activity.TimeBegin <= lastDayOfMonth)
+            .ToListAsync();
+
+        return ModelMapper.MapToListModel(entities);
+    }
+
+    /// <summary>
+    /// Returns a collection of activities that began (!) in the previous month.
+    /// </summary>
+    /// <remarks>Previous month is considered from the first day.</remarks>
+    /// <returns>Collection of ActivityListModels that began in the previous month.</returns>
+    public async Task<IEnumerable<ActivityListModel>> GetPreviousMonth()
+    {
+        DateTime dt = DateTime.Now.AddMonths(-1);
+
+        var firstDayOfMonth = new DateTime(dt.Year, dt.Month, 1);
+        var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddSeconds(-1);
+
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        List<Activity> entities = await uow
+            .GetRepository<Activity, ActivityEntityMapper>()
+            .Get()
+            .Where(activity => activity.TimeBegin >= firstDayOfMonth && activity.TimeBegin <= lastDayOfMonth)
+            .ToListAsync();
+
+        return ModelMapper.MapToListModel(entities);
+    }
+
+    /// <summary>
+    /// Returns a collection of activities that began (!) in the past year.
+    /// </summary>
+    /// <remarks>Past year is considered from the first of January, not last 365 days.</remarks>
+    /// <returns>Collection of ActivityListModels that began in the past year.</returns>
+    public async Task<IEnumerable<ActivityListModel>> GetPastYear()
+    {
+        DateTime dt = DateTime.Now;
+
+        var firstDayOfYear = new DateTime(dt.Year, 1, 1);
+        var lastDayOfYear = firstDayOfYear.AddYears(1).AddSeconds(-1);
+
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+        List<Activity> entities = await uow
+            .GetRepository<Activity, ActivityEntityMapper>()
+            .Get()
+            .Where(activity => activity.TimeBegin >= firstDayOfYear && activity.TimeBegin <= lastDayOfYear)
+            .ToListAsync();
+
+        return ModelMapper.MapToListModel(entities);
     }
 }
--- Project/Tests/Project.BL.Tests/ActivityFacadeTests.cs
+++ Project/Tests/Project.BL.Tests/ActivityFacadeTests.cs
@@ -70,4 +70,157 @@ public sealed class ActivityFacadeTests : FacadeTestsBase
         Assert.Contains(listModel, returnedModel);
 
     }
+
+    [Fact]
+    public async Task GetAll_Filter_FromPastWeek()
+    {
+        // None of the seeded activities have start date from past month
+        var fromSeeds = await _facadeSUT.GetPastWeek();
+        Assert.Empty(fromSeeds);
+
+        // We add a new activity that took place today
+        var newEntity = ActivitySeeds.NotSeededPastWeek;
+
+        // We add a new activity that took place on the first day of previous month
+        var oldEntity = ActivitySeeds.NotSeededPreviousMonth;
+
+        // GetPastWeek returns list model, so we'll need both
+        var listModel = ActivityModelMapper.MapToListModel(newEntity);
+        var detailModel = ActivityModelMapper.MapToDetailModel(newEntity);
+
+        var oldDetailModel = ActivityModelMapper.MapToDetailModel(oldEntity);
+
+        await _facadeSUT.SaveAsync(detailModel);
+        await _facadeSUT.SaveAsync(oldDetailModel);
+
+        var fromAdded = await _facadeSUT.GetPastWeek();
+
+        // Output of GetPastWeek is not empty
+        Assert.NotEmpty(fromAdded);
+
+        // Output has exactly one activity
+        Assert.True(fromAdded.Count() == 1);
+
+        var returnedModel = fromAdded.First();
+        FixIds(listModel, returnedModel);
+
+        // It should be the new one
+        DeepAssert.Equal(listModel, returnedModel);
+    }
+
+    [Fact]
+    public async Task GetAll_Filter_FromPastMonth()
+    {
+        // None of the seeded activities have start date from past month
+        var fromSeeds = await _facadeSUT.GetPastMonth();
+        Assert.Empty(fromSeeds);
+
+        // We add a new activity that took place on the first day of this month
+        var newEntity = ActivitySeeds.NotSeededPastMonth;
+
+        // We add a new activity that took place on the first day of previous month
+        var oldEntity = ActivitySeeds.NotSeededPreviousMonth;
+
+        // GetPastWeek returns list model, so we'll need both
+        var listModel = ActivityModelMapper.MapToListModel(newEntity);
+        var detailModel = ActivityModelMapper.MapToDetailModel(newEntity);
+
+        var oldDetailModel = ActivityModelMapper.MapToDetailModel(oldEntity);
+
+        await _facadeSUT.SaveAsync(detailModel);
+        await _facadeSUT.SaveAsync(oldDetailModel);
+
+        var fromAdded = await _facadeSUT.GetPastMonth();
+
+        // Output of GetPastWeek is not empty
+        Assert.NotEmpty(fromAdded);
+
+        // Output has exactly one activity
+        Assert.True(fromAdded.Count() == 1);
+
+        var returnedModel = fromAdded.First();
+        FixIds(listModel, returnedModel);
+
+        // It should be the new one
+        DeepAssert.Equal(listModel, returnedModel);
+    }
+
+    [Fact]
+    public async Task GetAll_Filter_FromPreviousMonth()
+    {
+        // None of the seeded activities have start date from previous month
+        var fromSeeds = await _facadeSUT.GetPreviousMonth();
+        Assert.Empty(fromSeeds);
+
+        // We add a new activity that took place on the first day of previous month
+        var newEntity = ActivitySeeds.NotSeededPreviousMonth;
+
+        // We add a new activity that took place on the first day of previous year
+        var oldEntity = ActivitySeeds.NotSeededPreviousYear;
+
+        // GetPastWeek returns list model, so we'll need both
+        var listModel = ActivityModelMapper.MapToListModel(newEntity);
+        var detailModel = ActivityModelMapper.MapToDetailModel(newEntity);
+
+        var oldDetailModel = ActivityModelMapper.MapToDetailModel(oldEntity);
+
+        await _facadeSUT.SaveAsync(detailModel);
+        await _facadeSUT.SaveAsync(oldDetailModel);
+
+        var fromAdded = await _facadeSUT.GetPreviousMonth();
+
+        // Output of GetPastWeek is not empty
+        Assert.NotEmpty(fromAdded);
+
+        // Output has exactly one activity
+        Assert.True(fromAdded.Count() == 1);
+
+        var returnedModel = fromAdded.First();
+        FixIds(listModel, returnedModel);
+
+        // It should be the new one
+        DeepAssert.Equal(listModel, returnedModel);
+    }
+
+    [Fact]
+    public async Task GetAll_Filter_FromPastYear()
+    {
+        // None of the seeded activities have start date from past year
+        var fromSeeds = await _facadeSUT.GetPastYear();
+        Assert.Empty(fromSeeds);
+
+        // We add a new activity that took place on the first day of past year
+        var newEntity = ActivitySeeds.NotSeededPastYear;
+
+        // We add a new activity that took place on the first day of previous year
+        var oldEntity = ActivitySeeds.NotSeededPreviousYear;
+
+        // GetPastWeek returns list model, so we'll need both
+        var listModel = ActivityModelMapper.MapToListModel(newEntity);
+        var detailModel = ActivityModelMapper.MapToDetailModel(newEntity);
+
+        var oldDetailModel = ActivityModelMapper.MapToDetailModel(oldEntity);
+
+        await _facadeSUT.SaveAsync(detailModel);
+        await _facadeSUT.SaveAsync(oldDetailModel);
+
+        var fromAdded = await _facadeSUT.GetPastYear();
+
+        // Output of GetPastWeek is not empty
+        Assert.NotEmpty(fromAdded);
+
+        // Output has exactly one activity
+        Assert.True(fromAdded.Count() == 1);
+
+        var returnedModel = fromAdded.First();
+        FixIds(listModel, returnedModel);
+
+        // It should be the new one
+        DeepAssert.Equal(listModel, returnedModel);
+    }
+
+    private static void FixIds(ActivityListModel expectedModel, ActivityListModel returnedModel)
+    {
+        returnedModel.Id = expectedModel.Id;
+    }
 }
