d7a022e551de300d5a62257f334e9a72f5ca0907
Matěj Toul <mates097@seznam.cz>
2023-04-27 20:37:50 +0200

    Prepared the MauiProgram, layer installers and everything they need

---
+++ Project/Project.App/DALInstaller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Maui.Controls;
+using Project.DAL;
+using Project.DAL.Factories;
+using Project.DAL.Mappers;
+
+namespace Project.App
+{
+    public static class DALInstaller
+    {
+        public static IServiceCollection AddDALServices(this IServiceCollection services)
+        {
+            // TODO: Turn off seeding and recreating for final product
+            DALOptions dalOptions = new()
+            {
+                Sqlite = new()
+                {
+                    DatabaseName = "ics_project.db",
+                    Enabled = true,
+                    RecreateDatabaseEachTime = true,
+                    SeedDemoData = true
+                }
+            };
+
+            services.AddSingleton<DALOptions>(dalOptions);
+
+            string databaseFilePath = Path.Combine(FileSystem.AppDataDirectory, dalOptions.Sqlite.DatabaseName);
+            services.AddSingleton<IDbContextFactory<DatabaseContext>>(provider => new DbContextSqlLiteFactory(databaseFilePath, dalOptions.Sqlite.SeedDemoData));
+            services.AddSingleton<IDbMigrator, SqliteDbMigrator>();
+
+            services.AddSingleton<ActivityEntityMapper>();
+            services.AddSingleton<ProjectEntityMapper>();
+            services.AddSingleton<UserEntityMapper>();
+            services.AddSingleton<UserProjectEntityMapper>();
+
+            return services;
+        }
+    }
+}
--- Project/Project.DAL/Seeds/UserSeeds.cs
+++ Project/Project.DAL/Seeds/UserSeeds.cs
@@ -15,6 +15,15 @@ namespace Project.DAL.Seeds
                 @"https://w7.pngwing.com/pngs/753/432/png-transparent-user-profile-2018-in-sight-user-conference-expo-business-default-business-angle-service-people-thumbnail.png"
         };
 
+        public static readonly User DefaultUser2 = new()
+        {
+            Id = Guid.Parse("A357D7DF-9DF0-4787-B489-A97004034067"),
+            FirstName = "Matěj",
+            LastName = "Toul",
+            PhotoUrl =
+                @"https://w7.pngwing.com/pngs/753/432/png-transparent-user-profile-2018-in-sight-user-conference-expo-business-default-business-angle-service-people-thumbnail.png"
+        };
+
         public static void Seed(this ModelBuilder modelBuilder) =>
             modelBuilder.Entity<User>().HasData(
                     DefaultUser with { 
