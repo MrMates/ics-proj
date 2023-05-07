f5a44c4e05393d02423b52f2a0b9b984c6c8e293
Miky537 <xkopci01@vutbr.cz>
2023-05-07 16:38:45 +0200

    UserProfilePage now pre-fill first name and last name.

--- Project/Project.App/ViewModels/User/UserProfileViewModel.cs
+++ Project/Project.App/ViewModels/User/UserProfileViewModel.cs
@@ -12,8 +12,8 @@ public partial class UserProfileViewModel : ViewModelBase
     private readonly INavigationService _navigationService;
 
 
-    public string FirstName { get; set; }
-    public string SurName { get; set; }
+    public string FirstName { get; set; } = (string) Shell.Current.Resources["firstName"];
+    public string SurName { get; set; } = (string)Shell.Current.Resources["surName"];
     public Color FrameBackgroundColor { get; set; } = Color.FromRgba(217, 217, 217, 255);
     public string ImageFileString { get; set; }
     private ImageSource _profilePicSource = (string)Shell.Current.Resources["userPic"];
